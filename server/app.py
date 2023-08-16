b8d5bb05d4cbab19b956a914828a8592ad07db53
Kyle <kylekibbe99@gmail.com>
2023-08-15 22:50:23 -0700

    final commit

--- server/app.py
+++ server/app.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python3
-
 from flask import Flask
-
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<parameter>')
+def print_string(parameter):
+    print (parameter)
+    return parameter
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    return '\n'.join(map(str, range(parameter))) + '\n'
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == 'div':
+        return str(float(num1) / num2)
+    elif operation == '%':
+        return str(num1 % num2)
+    else:
+        return 'Invalid operation'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
